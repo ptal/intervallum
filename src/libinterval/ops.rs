2341169e09aa6a5669880647b0be87914851c486
Pierre Talbot <ptalbot@hyc.io>
2015-04-04 00:00:29 +0200

    Change package name to intervallum (interval in latin) because interval is already taken in crates.io.

--- src/libinterval/ops.rs
+++ src/libinterval/ops.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Interval specific operations.
+//! Interval and bound specific operations.
 
 use std::num::Int;
 
